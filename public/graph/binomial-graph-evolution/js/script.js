7d836222b4b8e5810b3359ce26e596a057e084c1
Faisal Bagalagel <uultradude@gmail.com>
2021-04-01 16:08:46 -0400

    fixed binomial graph bug

--- public/graph/binomial-graph-evolution/js/script.js
+++ public/graph/binomial-graph-evolution/js/script.js
@@ -40,9 +40,22 @@ function btnOnClick(btnId) {
         }
         console.log(nodes);
         //step 1: create lexographic ranking of the index pairs
-        const combinations = k_combinations(nodes, 2)
-       
+        //const combinations = k_combinations(nodes, 2)
 
+        //THIS IS WHERE WE LOAD THE COMBINATION ARRAY
+       	combinations = comb(nodes);
+       	//THIS IS WHERE WE PRINT THE COMBINATION ARRAY AND IT IS NOT IN ORDER
+       	console.log(combinations);
+
+       	//combinations.sort();
+       	//combinations = array.sort(function(a, b) {
+  		//if (a[0] == b[0]) {
+    	//	return a[1] - b[1];
+  		//}
+  		//return b[0] - a[0];
+		//});
+
+       	console.log(combinations)
         //step 2: set k=0, i=0
         i=0;
         k=0;
@@ -56,6 +69,12 @@ function btnOnClick(btnId) {
             	r += 1;
             }
             //step 4: join vertices
+
+             if(combinations[r] == undefined){
+                //combinations.splice(r,r+1);
+                continue;
+            }
+            
             let newLink = {source: combinations[r][0], target: combinations[r][1]}
             edges.push(newLink);
             rvalues.push(r);
@@ -84,7 +103,7 @@ function btnOnClick(btnId) {
         	}
         	for(i=0; i<links.length; i++){
         		if(edge.target.id == links[i].target.id && edge.source.id == links[i].source.id){
-    				document.getElementById('Graph info').value += "This Edge has already been added\n";
+    				//document.getElementById('Graph info').value += "This Edge has already been added\n";
     				pass = 0;
         		}
         	}
@@ -109,59 +128,21 @@ function updateLabels() {
     document.getElementById('edgeLabel').innerHTML = "Edges: "+links.length;
 }
 
-function k_combinations(set, k) {
-    var i, j, combs, head, tailcombs;
-    
-    // There is no way to take e.g. sets of 5 elements from
-    // a set of 4.
-    if (k > set.length || k <= 0) {
-        return [];
-    }
-    
-    // K-sized set has only one K-sized subset.
-    if (k == set.length) {
-        return [set];
-    }
-    
-    // There is N 1-sized subsets in a N-sized set.
-    if (k == 1) {
-        combs = [];
-        for (i = 0; i < set.length; i++) {
-            combs.push([set[i]]);
-        }
-        return combs;
-    }
-    
-    // Assert {1 < k < set.length}
-    
-    // Algorithm description:
-    // To get k-combinations of a set, we want to join each element
-    // with all (k-1)-combinations of the other elements. The set of
-    // these k-sized sets would be the desired result. However, as we
-    // represent sets with lists, we need to take duplicates into
-    // account. To avoid producing duplicates and also unnecessary
-    // computing, we use the following approach: each element i
-    // divides the list into three: the preceding elements, the
-    // current element i, and the subsequent elements. For the first
-    // element, the list of preceding elements is empty. For element i,
-    // we compute the (k-1)-computations of the subsequent elements,
-    // join each with the element i, and store the joined to the set of
-    // computed k-combinations. We do not need to take the preceding
-    // elements into account, because they have already been the i:th
-    // element so they are already computed and stored. When the length
-    // of the subsequent list drops below (k-1), we cannot find any
-    // (k-1)-combs, hence the upper limit for the iteration:
-    combs = [];
-    for (i = 0; i < set.length - k + 1; i++) {
-        // head is a list that includes only our current element.
-        head = set.slice(i, i + 1);
-        // We take smaller combinations from the subsequent elements
-        tailcombs = k_combinations(set.slice(i + 1), k - 1);
-        // For each (k-1)-combination we join it with the current
-        // and store it to the set of k-combinations.
-        for (j = 0; j < tailcombs.length; j++) {
-            combs.push(head.concat(tailcombs[j]));
-        }
-    }
-    return combs;
+// THIS IS THE COMBINATION FUNCTION WE ARE USING (ONE THAT IS NOT PUSHING VALUES IN SORTED ORDER)
+function comb(set){
+	var combs = []
+	for (var node = 0; node < set.length; node++) {
+		console.log(set[node]);
+		for (var otherNode = node + 1; otherNode < set.length; otherNode++) {
+			var pair = [set[node], set[otherNode]]
+			console.log("others")
+			//console.log(set[otherNode])
+			console.log(pair);
+				//if(!combs.includes(pair)){
+			combs.push(pair);
+				//}
+		}
+	}
+	//console.log(combs)
+	return combs;
 }
