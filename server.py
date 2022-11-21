36b5e7c7265e4f7715a7926d6aa06a96e6430f25
Jeremie Bornais <jeremiejbornais@gmail.com>
2022-11-21 20:45:53 +0000

    pushing basic flask start

---
+++ server.py
@@ -0,0 +1,29 @@
+import os
+import json
+import sys
+from flask import Flask, render_template
+
+# getting config details
+with open('config.json') as f:
+    data = json.load(f)
+
+# initializing Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = data['secret_key']
+
+@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+# running the site
+if __name__=='__main__':
+    # run this command with any additional arg to run in production
+    if len(sys.argv) > 1:
+        print('<< PROD >>')
+        os.system(f"gunicorn -b '127.0.0.1:{data['port']}' server:app")
+    # or just run without an additional arg to run in debug
+    else:
+        print('<< DEBUG >>')
+        app.run(debug=True)
